ab0b737b00e3aaeddaf83823d914c54b9fcab55d
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-08-30 23:18:57 +0200

    Added a Setting Class with certain configuration

--- src/main/java/com/example/alieninvasiongame/Main.java
+++ src/main/java/com/example/alieninvasiongame/Main.java
@@ -1,15 +1,18 @@
 package com.example.alieninvasiongame;
 
+import com.example.alieninvasiongame.settings.Settings;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage){
         stage.setTitle("Galactic Defender");
@@ -18,10 +21,13 @@ public class Main extends Application {
         Image icon = new Image("C:\\Users\\hifi\\OneDrive\\Desktop\\Java work\\AlienInvasionGame\\src\\main\\resources\\com\\example\\alieninvasiongame\\invasion.png");
         stage.getIcons().add(icon);
 
+        // Initialized the settings
+        Settings settings = new Settings();
+
         // Create an empty black scene with a root node
         Group root = new Group();
-        Scene scene = new Scene(root, 800, 600);
-        scene.setFill(Color.BLACK);
+        Scene scene = new Scene(root, settings.getHeight(), settings.getWidth());
+        scene.setFill((Paint) settings.getColors().get("black"));
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/alieninvasiongame/settings/Settings.java
@@ -0,0 +1,30 @@
+package com.example.alieninvasiongame.settings;
+
+import javafx.scene.paint.Color;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Settings {
+    private final int WIDTH = 600;
+    private final int HEIGHT = 800;
+    private Map colors = new HashMap<>();
+
+    public Settings(){
+        this.colors.put("white", Color.WHITE);
+        this.colors.put("black", Color.BLACK);
+    }
+    public Map getColors() {
+        return this.colors;
+    }
+
+    public int getHeight() {
+        return this.HEIGHT;
+    }
+
+    public int getWidth() {
+        return this.WIDTH;
+    }
+
+
+}
